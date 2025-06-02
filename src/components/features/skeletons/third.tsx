affe8a51c72ae3a998e12640fc52961d2179b2f2
Christian Seyboth <120297450+christianseyboth@users.noreply.github.com>
2025-06-02 08:46:22 +0200

    Updated third.tsx

--- src/components/features/skeletons/third.tsx
+++ src/components/features/skeletons/third.tsx
@@ -67,7 +67,7 @@ export const SkeletonThree = () => {
       repeat: Infinity,
       repeatDelay: 1,
     });
-  }, []);
+  }, [sequence]);
   return (
     <div className="p-8 overflow-hidden h-full relative flex items-center justify-center">
       <div className="flex flex-row flex-shrink-0 justify-center items-center gap-2">
