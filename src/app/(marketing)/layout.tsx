f579f43ffb3143901fe1b02800e147de0b022b24
Christian Seyboth <120297450+christianseyboth@users.noreply.github.com>
2025-06-17 14:52:40 +0200

    Prod Fix

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,20 +1,15 @@
 import { NavBar } from '@/components/layout/navbar';
 import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
-import { createClient } from '@/utils/supabase/server';
 
 export default async function MarketingLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const supabase = await createClient();
-    const { data, error } = await supabase.auth.getUser();
-    const user = data.user;
-
     return (
         <div style={{ position: 'relative', minHeight: '100vh' }}>
-            <NavBar user={user} />
+            <NavBar />
             <main style={{ position: 'relative' }}>{children}</main>
             <Footer />
             <Toaster />
